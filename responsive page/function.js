bd2ee0275ebacb0b85949a726836b635c48f47d9
Babatunde Jonathan Leke <108595563+Leke05@users.noreply.github.com>
2022-08-03 06:48:45 +0100

    Add files via upload

---
+++ responsive page/function.js
@@ -0,0 +1,21 @@
+function aboutMe(){
+    alert('Name: Babatunde Jonathan');
+    alert('Dob: May 29');
+    alert('Hobby: Football');
+    alert('Religion: Christainity');
+    alert('State Of Origin: Oyo State');
+    alert('Place Of Birth: Ogbomoso');
+    alert('Favorite Sport: Football');
+    alert('Favorite Food: Swallo');
+    alert('Occupation: Developer');
+}
+
+const student = {
+    name: "Philip",
+    mark: {
+        physics: 70,
+        biology: 65,
+    }
+}
+
+console.log(student.mark.biology);
